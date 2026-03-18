9b080083a72ad7f1553864344e3a69651478bc3b
yashvi275 <yashvi25csu275@ncuindia.edu>
2026-03-18 22:08:02 +0530

    Create q35.cpp

---
+++ q35.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3], transpose[3][3];
+
+    cout << "Enter elements of 3x3 matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Compute transpose
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    // Display transpose
+    cout << "Transpose of the matrix:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
